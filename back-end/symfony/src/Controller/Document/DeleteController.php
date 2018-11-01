7a51a5901240b5d91b78346ddfb185dfbf48467d
Julius Koronci <jk@web-solutions.sk>
2018-11-01 14:59:11 +0100

    #REFACTOR - remove true left from testing

--- back-end/symfony/src/Controller/Document/DeleteController.php
+++ back-end/symfony/src/Controller/Document/DeleteController.php
@@ -34,7 +34,7 @@ class DeleteController extends BaseController
         $document = $this->getDoctrine()->getRepository(Document::class)->findOneBy(['slug' => $slug]);
 
         $removed = DocumentService::removeDocument($document, $this->getSystemPath());
-        if (true) {
+        if (false === $removed) {
             return $this->json(['error' => 'Unable to delete file'], 400);
         }
 
